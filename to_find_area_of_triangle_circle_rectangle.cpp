c3292fa4ddf9fd966197e72d7b11c9a625054fa4
ARVIND SINGH <arvind05singh9617@gmail.com>
2026-02-18 09:53:15 +0530

    Create to_find_area_of_triangle_circle_rectangle.cpp

---
+++ to_find_area_of_triangle_circle_rectangle.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+#define Pi 3.14
+float area_circle(float r){
+    return Pi*r*r;
+}
+float area_rectangle(float w, float l){
+    return w*h;
+}
+float area_triangle(float b, float h){
+    return 0.5*b*h;
+}
+int main(){
+    float radius, height, length, width, base;
+    cout<<"Enter the radius of the circle : "<<endl;
+    cin>>radius;
+    cout<<"Area of circle is "<<area_circle(radius)<<endl;
+    cout<<"Enter the height and base of the triangle : "<<endl;
+    cin>>height>>base;
+    cout<<"Area of the triangle is "<<area_triangle(height,base)<<endl;
+    cout<<"Enter the width and height of the rectangle : "<<endl;
+    cin>>width>>height;
+    cout<<"Area of the rectangle is "<<area_rectangle<<endl;
+    return 0;
+}
