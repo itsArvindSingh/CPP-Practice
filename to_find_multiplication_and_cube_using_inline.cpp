cd0015a7528d06e8548f830f709577291641cde7
ARVIND SINGH <arvind05singh9617@gmail.com>
2026-02-19 09:20:47 +0530

    Fix cube calculation in inline.cpp

--- to_find_multiplication_and_cube_using_inline.cpp
+++ to_find_multiplication_and_cube_using_inline.cpp
@@ -13,6 +13,6 @@ int main(){
     cout<<"Multiplication of "<<num1<<" and "<<num2<<" is "<<multiply(num1,num2)<<endl;
     cout<<"Enter a number to find the cube of the number : "<<endl;
     cin>>num3;
-    cout<<"cube of "<<num3<< " is "<<cube(num)
+    cout<<"cube of "<<num3<< " is "<<cube(num3);
     return 0;
 }
