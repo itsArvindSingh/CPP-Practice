0ffd0270e256dd515b1466aa0b13c60cb2677a16
ARVIND SINGH <arvind05singh9617@gmail.com>
2026-03-24 04:10:06 +0530

    updated code

---
+++ operator overloading/Unary overloading/2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class abc{
+    int m,n;
+    public:
+    abc(){
+        m=8;
+        n=9;
+    }
+    void show(){
+        cout<<"m : "<<m<<endl<<"n : "<<n<<endl;
+    }
+    void operator--(int){//int for post increment or decrement
+        m--;
+        n--;
+    }
+};
+int main(){
+    abc x;
+    x.show();
+    x--;
+    x.show();
+    return 0;
+}
