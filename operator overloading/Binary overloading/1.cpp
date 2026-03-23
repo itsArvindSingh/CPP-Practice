187ad48da1090b3ea253f5e48a2662098e0b0b3c
ARVIND SINGH <arvind05singh9617@gmail.com>
2026-03-24 04:22:20 +0530

    updated code

---
+++ operator overloading/Binary overloading/1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class abc{
+    int m,n;
+    public:
+    abc(){
+        m=8;
+        n=9;
+    }
+    void show(){
+        cout<<"m : "<<m<<endl<<"n : "<<n<<endl;
+    }
+    abc operator+(abc y){
+        abc b;
+        b.m=m+y.m;
+        b.n=n+y.n;
+        return b;
+    }
+};
+int main(){
+    abc x,y,z;
+    z=x+y;
+    z.show();
+    return 0;
+}
