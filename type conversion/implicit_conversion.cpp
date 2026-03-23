4b1b9f2172208300e6b4a38a896fc41840948add
ARVIND SINGH <arvind05singh9617@gmail.com>
2026-03-24 05:28:55 +0530

    updated code

---
+++ type conversion/implicit_conversion.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x=23;
+    char y='k';
+    float z=5.3;
+    x=x+y;
+    z=y+z;
+    cout<<"x is "<<x<<", y is "<<y<<", z is "<<z<<endl;
+    return 0;
+}
