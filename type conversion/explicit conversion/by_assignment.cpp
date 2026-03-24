33d8ecfd491a46230a145ad122b57e8b6e58275c
ARVIND SINGH <arvind05singh9617@gmail.com>
2026-03-24 11:17:30 +0530

    updated code

---
+++ type conversion/explicit conversion/by_assignment.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char p='p';
+    int a=23+ int (p);
+    cout<<a;
+    return 0;
+}
---
+++ type conversion/explicit conversion/using cast operator/static_cast.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float f=34.23;
+    int a=static_cast<int>(f);
+    cout<<a;
+    return 0;
+}
